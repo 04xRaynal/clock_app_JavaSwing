502ded085e86202de56c28e4d6b895abcae77f59
04xRaynal <dcunha.raynal4@gmail.com>
2021-03-17 15:03:36 +0530

    removed unused import
    ImageIcon import was removed

--- src/clock_panel/ClockMain.java
+++ src/clock_panel/ClockMain.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
